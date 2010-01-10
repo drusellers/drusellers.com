6b5d154073de808785698835814fdde8e4bdf091
drusellers <dru@drusellers.com>
2010-01-10 06:59:12 -0600

    tracking website

---
+++ timeline/local_data.js
@@ -0,0 +1,42 @@
+var timeline_data = {  // save as a global variable
+'dateTimeFormat': 'iso8601',
+'wikiURL': "http://simile.mit.edu/shelf/",
+'wikiSection': "Simile Cubism Timeline",
+
+'events' : [
+        {'start': '1997',
+		'end':'2001',
+        'title': 'College',
+        'description': 'by Lyonel Feininger, American/German Painter, 1871-1956',
+        'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
+        'link': 'http://www.bakeru.edu'
+        },
+		{
+			'start':'1997',
+			'title':'Takes HTML course',
+			'description':'whoot'
+		},
+		{
+			'start':'2001',
+			'title':'Discovers Macromedia UltraDev'
+		},
+		{
+			'start':'2002',
+			'title':'discovers OO via PHP'
+		},
+		{
+			'start':'2003',
+			'title':'first .Net programming',
+			'description':'vbc.exe'
+		},
+		{
+			'start':'2004',
+			'title':'Gets VS.Net for the first time'
+		},
+		{
+			'start':'2005',
+			'title':'Purchased CSLA'
+		}
+		
+]
+}
