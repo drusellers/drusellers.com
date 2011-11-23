605ae57ade0f627287ff25c660175b2ef5152eb1
drusellers <dru@drusellers.com>
2011-11-23 16:54:50 -0600

    checkpoint

--- runserver.py
+++ runserver.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template
 from flaskext.assets import Environment, Bundle
+import feedparser
 
 app = Flask(__name__)
 assets = Environment(app)
 app.debug = True
 
-sass = Bundle('styles/base.sass', filters='sass', output='styles/base.css',
-        debug=False)
+app.config['LESS_PATH']='/usr/local/bin/lessc'
 
-assets.register('all-css', sass, output='styles/gen.css')
+less = Bundle('styles/base.less', filters='less', output='styles/base.css',
+    debug=True)
+
+assets.register('all-css', less, output='styles/gen.css')
+
+def take(num, items):
+    for i in range(num):
+        yield items[i]
+
+#blog posts - not sure codebetter has individual feeds
+#posts = feedparser.parse('http://codebetter.com/drusellers/feed/').entries
+#tweets
+#tweets = feedparser.parse('http://twitter.com/statuses/user_timeline/drusellers.rss?count=10').entries
+#github
+#commits = feedparser.parse('https://github.com/drusellers.private.actor.atom?token=1dbb0464b79d688827f66a9120c32b10').entries
+#wods?
+#log = feedparser.parse('http://drusellers.tumblr.com/rss').entries
 
 @app.route('/')
 def hello():
-    #blog posts
-    #tweets
-    #github?
-    #wods?
+
     return render_template('index.html')
+#            tweets=take(10, tweets),
+#            commits=take(10, commits),
+#            posts=take(10, posts),
+#            log=take(10, log))
 
 if __name__ == '__main__':
     app.run()
