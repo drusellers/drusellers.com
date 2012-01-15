fda378c5127de3c0bc3cf54bf35e151cf027565b
drusellers <dru@drusellers.com>
2012-01-15 15:16:11 -0600

    fixing the app.run command

--- runserver.py
+++ runserver.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flaskext.assets import Environment, Bundle
 import feedparser
@@ -46,4 +47,5 @@ def send_email(name, email, message):
     pass
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT","5000"))
+    app.run(host='0.0.0.0',port=port)
