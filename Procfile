694a7278c158f9f58ac4edf874229534c61f9ab5
drusellers <dru@drusellers.com>
2012-02-05 11:29:25 -0600

    moving to node.js - frigging css almost done - also moved to scss

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python runserver.py
+web: node app.js
--- actions/index.js
+++ actions/index.js
@@ -4,22 +4,4 @@ module.exports = function(app) {
       title: 'Express'
     })
   });
-
-  app.get('/leaderboard', function(req, res){
-    res.render('leaderboard', {
-      atheletes : [
-        {name:'Ingrid',rank:1,box:'CrossFit Central'},
-        {name:'Winchester', rank:2, box:'CrossFitCentral'}
-      ]
-    });
-  });
-
-  app.get('/dataentry', function(req, res) {
-    res.render('dataentry', {
-      atheletes : [
-        {name:'Ingrid',rank:1,box:'CrossFit Central'},
-        {name:'Winchester', rank:2, box:'CrossFitCentral'}
-      ]
-    });
-  });
 };
